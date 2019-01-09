d553ec920944fce3e16ec39896314ae285a7e5a9
Ashraf Alam <joy_csharp@yahoo.com>
2019-01-09 22:47:53 +0800

    Update ValuesController.cs

--- WebApi/Eisk.WebApi/Controllers/ValuesController.cs
+++ WebApi/Eisk.WebApi/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace Eisk.WebApi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", DateTime.UtcNow.ToString() };
+            return new string[] { "value1", "value2..", DateTime.UtcNow.ToString() };
         }
 
         // GET api/values/5
