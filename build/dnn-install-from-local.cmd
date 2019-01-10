ec0a8d92125aa3d08fa92eaf1f684f2f8a726604
Ashraf Alam <joy_csharp@yahoo.com>
2019-01-10 18:20:47 +0800

    Update dnn-install-from-local.cmd

--- build/dnn-install-from-local.cmd
+++ build/dnn-install-from-local.cmd
@@ -1,9 +1,9 @@
 echo $$$$$$$$$$$$$$$$$$$$ installing template {nuspec - package id / path} 
 dotnet new -i  ..\Eisk.WebApi.TemplatePack\bin\Debug\templates\*.nupkg
 
-rmdir .\dnn-local-template-render /s /q
-mkdir dnn-local-template-render
-cd dnn-local-template-render
+rmdir .\content /s /q
+mkdir content
+cd content
 
 echo $$$$$$$$$$$$$$$$$$$$ create content from template {template.json - shortname}
 dotnet new eiskwebapi -n App
