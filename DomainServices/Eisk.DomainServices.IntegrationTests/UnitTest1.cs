523204cbf9cd54da42431ddf12895a4c003fb87a
AshrafAlam <joy_csharp@yahoo.com>
2019-01-15 21:22:08 +0800

    -- added domain and data service projects and test projects

---
+++ DomainServices/Eisk.DomainServices.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Eisk.DomainServices.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ DomainServices/Eisk.DomainServices/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Eisk.DomainServices
+{
+    public class Class1
+    {
+    }
+}
