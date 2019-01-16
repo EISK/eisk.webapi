e32888febcaa8c85868ce378f47e67b899636fac
ashrafalam <joy_csharp@yahoo.com>
2019-01-16 14:32:33 +0800

    -- added integration tests for domain

---
+++ DomainServices/Eisk.DomainServices.IntegrationTests/EmployeeDomainServiceIntegrationTests.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Linq;
+using Eisk.DataServices.EntityFrameworkCore;
+using Eisk.Domains.Employee;
+using Eisk.DomainServices.IntegrationTests;
+using Services.DomainServices;
+using Test.Core.TestBases;
+using Xunit;
+
+namespace IntegrationTests.DomainServiceTests
+{
+    public class EmployeeDomainServiceIntegrationTests : DomainServiceBaseIntegrationTests<Employee, int>
+    {
+        #region Helpers
+
+        public EmployeeDomainServiceIntegrationTests() :
+            this(new EmployeeDomainService(Factory_DataService()))
+        {
+        }
+
+        EmployeeDomainServiceIntegrationTests(EmployeeDomainService employeeDomainService) :
+            base(x => x.Id, employeeDomainService)
+        {
+            _employeeDomainService = employeeDomainService;
+        }
+
+        private readonly EmployeeDomainService _employeeDomainService;
+
+        static EmployeeDataService Factory_DataService()
+        {
+            var testDb = new TestDbContextFactory();
+
+            EmployeeDataService employeeDataService = new EmployeeDataService(testDb.CreateDbContext());
+
+            return employeeDataService;
+        }
+
+        #endregion
+
+        [Fact]
+        public void GetByFirstName_ValidFirstNamePassed_ShouldBeFoundInQuery()
+        {
+            //Arrange
+            var firstName = "John";
+            var objectToAdd = Factory_Entity(x => x.FirstName = firstName);
+            _employeeDomainService.Add(objectToAdd);
+
+            //Act
+            var returnEmployee = _employeeDomainService.GetByFirstName(firstName);
+
+            //Assert
+            Assert.NotNull(returnEmployee);
+            Assert.Equal(firstName, returnEmployee.FirstOrDefault().FirstName);
+        }
+    }
+}
---
+++ DomainServices/Eisk.DomainServices.IntegrationTests/Setup/TestDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Eisk.DataServices.EntityFrameworkCore.DataContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace Eisk.DomainServices.IntegrationTests
+{
+    public class TestDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext()
+        {
+            return CreateDbContext(new[] { "" });
+        }
+
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseInMemoryDatabase("MyDb" + Guid.NewGuid());
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
