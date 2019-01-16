899c09d44ea283a3e1c0136624fca4e8085093bf
AshrafAlam <joy_csharp@yahoo.com>
2019-01-16 21:33:36 +0800

    updated employee data service tests

--- DataServices.EntityFrameworkCore/Eisk.DataServices.EntityFrameworkCore.IntegrationTests/EmployeeDataServiceTests.cs
+++ DataServices.EntityFrameworkCore/Eisk.DataServices.EntityFrameworkCore.IntegrationTests/EmployeeDataServiceTests.cs
@@ -4,36 +4,11 @@ using Xunit;
 
 namespace Eisk.DataServices.EntityFrameworkCore.IntegrationTests
 {
-    public class EmployeeDataServiceTests:EntityTestBase<Employee>
+    public class EmployeeDataServiceTests: DataServiceBaseIntegrationTests<Employee>
     {
-        #region Helpers
-
-        private readonly EmployeeDataService _employeeDataService = Factory_DataService();
-
-        static EmployeeDataService Factory_DataService()
+        public EmployeeDataServiceTests():base (new EmployeeDataService(new TestDbContextFactory().CreateDbContext()))
         {
-            var testDb = new TestDbContextFactory();
 
-            EmployeeDataService employeeDataService = new EmployeeDataService(testDb.CreateDbContext());
-
-            return employeeDataService;
-        }
-
-        #endregion
-
-        [Fact]
-        public void Add_1()
-        {
-            //Arrange
-            var objectToAdd = Factory_Entity(x => x.Id = 0);
-
-            //Act
-            var employeeDataService = Factory_DataService();
-            var returnEmployee = employeeDataService.Add(objectToAdd);
-
-            //Assert
-            Assert.NotEqual(0, returnEmployee.Id);
-            
         }
 
     }
