02efe66a29402286f52335dc7ac366bec4a4236d
AshrafAlam <joy_csharp@yahoo.com>
2019-01-18 22:20:37 +0800

    project refactored

--- DataServices.EntityFrameworkCore/Eisk.DataServices.EntityFrameworkCore.IntegrationTests/EmployeeDataServiceTests.cs
+++ DataServices.EntityFrameworkCore/Eisk.DataServices.EntityFrameworkCore.IntegrationTests/EmployeeDataServiceTests.cs
@@ -1,11 +1,12 @@
-using Eisk.DataServices.IntegrationTests;
+using Eisk.Domains.Employee;
 using Eisk.DomainServices.IntegrationTests;
+using Test.Core.TestBases;
 
 namespace Eisk.DataServices.EntityFrameworkCore.IntegrationTests
 {
-    public class EmployeeDataServiceTests: EmployeeDataServiceBaseIntegrationTests
+    public class EmployeeDataServiceTests: DataServiceBaseIntegrationTests<Employee, int>
     {
-        public EmployeeDataServiceTests():base (new EmployeeDataService(new InMemoryDbContextFactory().CreateDbContext()))
+        public EmployeeDataServiceTests():base (new EmployeeDataService(new InMemoryDbContextFactory().CreateDbContext()), x => x.Id)
         {
 
         }
