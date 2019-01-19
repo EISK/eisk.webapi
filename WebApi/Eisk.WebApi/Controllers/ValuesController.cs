d8897c264e24c4bdbc5ce7f1b584e114d52e92d5
AshrafAlam <joy_csharp@yahoo.com>
2019-01-19 15:28:21 +0800

    update

--- WebApi/Eisk.WebApi/Controllers/ValuesController.cs
+++ WebApi/Eisk.WebApi/Controllers/ValuesController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Eisk.EntityFrameworkCore.Setup;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis;
 
 namespace Eisk.WebApi.Controllers
 {
