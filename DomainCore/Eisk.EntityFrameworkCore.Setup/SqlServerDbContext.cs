1b236c96ccafa31db7a1b255b1ca1f28a4a6488b
AshrafAlam <joy_csharp@yahoo.com>
2019-01-19 18:03:41 +0800

    added support for sql server in web api

---
+++ DomainCore/Eisk.EntityFrameworkCore.Setup/SqlServerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Eisk.DataServices.EntityFrameworkCore.DataContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Eisk.EntityFrameworkCore.Setup
+{
+    public class SqlServerDbContext : AppDbContext
+    {
+        private IConfiguration _configuration;
+
+        public SqlServerDbContext(IConfiguration configuration) : base(new DbContextOptionsBuilder<AppDbContext>().Options)
+        {
+            _configuration = configuration;
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultSqlConnection"));
+        }
+    }
+}
