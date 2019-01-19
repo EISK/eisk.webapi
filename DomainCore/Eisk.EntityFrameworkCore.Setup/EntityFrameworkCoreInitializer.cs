9a68b4765bca85322c0147c858e28b07cba9fd09
AshrafAlam <joy_csharp@yahoo.com>
2019-01-20 03:12:15 +0800

    inmemory package reference updated

---
+++ DomainCore/Eisk.EntityFrameworkCore.Setup/EntityFrameworkCoreInitializer.cs
@@ -0,0 +1,31 @@
+﻿using Eisk.DataServices.EntityFrameworkCore.DataContext;
+using Eisk.Domains.Employee;
+using System.Linq;
+using Eisk.Test.Core.DataGen;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Eisk.EntityFrameworkCore.Setup
+{
+    public static class EntityFrameworkCoreInitializer
+    {
+        public static void Initialize(IServiceCollection services)
+        {
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+            IHostingEnvironment env = serviceProvider.GetService<IHostingEnvironment>();
+
+            if (env.IsDevelopment())
+            {
+                services.AddDbContext<InMemoryDbContext>();
+                services.AddTransient<AppDbContext, InMemoryDbContext>();
+            }
+            else
+            {
+                services.AddDbContext<SqlServerDbContext>();//pass iconfiguration
+                services.AddTransient<AppDbContext, SqlServerDbContext>();
+            }
+
+        }
+    }
+}
