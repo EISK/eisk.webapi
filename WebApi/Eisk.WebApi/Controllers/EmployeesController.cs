5d5a4f95f4d9487799ed4011b6226e3543331e73
AshrafAlam <joy_csharp@yahoo.com>
2019-02-04 14:03:37 +0800

    -- added swagger support

---
+++ WebApi/Eisk.WebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Eisk.Domains.Employee;
+using Eisk.DomainServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eisk.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private EmployeeDomainService _employeeDomainService;
+        public EmployeesController(EmployeeDomainService ctx)
+        {
+            _employeeDomainService = ctx;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _employeeDomainService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return _employeeDomainService.GetById(id);
+        }
+
+        [HttpPost]
+        public void Post(Employee employee)
+        {
+            _employeeDomainService.Add(employee);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, Employee employee)
+        {
+            _employeeDomainService.Update(id, employee);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _employeeDomainService.Delete(id);
+        }
+    }
+}
--- WebApi/Eisk.WebApi/Startup.cs
+++ WebApi/Eisk.WebApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Eisk.WebApi
 {
@@ -32,6 +33,20 @@ namespace Eisk.WebApi
 
             services.AddMvc();
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Eisk.WebApi", Version = "v1.0",
+                    Description = "EISK makes it easy to write scalable and secured web api on top of Microsoft's new cutting edge .net core technologies.",
+                    Contact = new Contact
+                    {
+                        Name = "Ashraf Alam",
+                        Email = string.Empty,
+                        Url = "https://eisk.github.io/eisk.webapi.docs"
+                    }
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +56,17 @@ namespace Eisk.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eisk.WebApi");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseMvc();
         }
