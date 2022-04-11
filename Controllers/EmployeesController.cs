87e5cf033e393471c296269f22f9440984d60f76
AshrafAlam <joy_csharp@yahoo.com>
2022-04-11 17:41:17 +0000

    Updated with minor changes.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿namespace Eisk.WebApi.Controllers
+{
+    using Eisk.Core.WebApi;
+    using Domains.Entities;
+    using DomainServices;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController : WebApiControllerBase<Employee, int>
+    {
+        public EmployeesController(EmployeeDomainService employeeDomainService):base(employeeDomainService)
+        {
+            
+        }
+    }
+}
