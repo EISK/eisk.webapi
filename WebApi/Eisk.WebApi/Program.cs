639075efba72fd56069a7e9fae541139150771ec
AshrafAlam <joy_csharp@yahoo.com>
2022-05-18 12:49:31 +0000

    Updated with minor changes.

--- WebApi/Eisk.WebApi/Program.cs
+++ WebApi/Eisk.WebApi/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo
     {
         Title = "Eisk.WebApi",
-        Version = "v9.0.25",
+        Version = "v9.0.32",
         Description = "EISK makes it easy to write scalable and secured web api on top of Microsoft's new cutting edge .net based technologies.",
         Contact = new OpenApiContact
         {
